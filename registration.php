407ac6910c8fe22645f0629934be6383fd284761
Richard BAYET <richard.bayet@smile.fr>
2017-03-16 11:45:41 +0100

    Initial module structure

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteTargetRule
+ * @author    Richard BAYET <richard.bayet@smile.fr>
+ * @copyright 2017 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Smile_ElasticsuiteTargetRule',
+    __DIR__
+);
