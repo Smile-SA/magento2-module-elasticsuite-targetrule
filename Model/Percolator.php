31c868019d446c714ad4f3b1db78e68778082722
Romain Ruaud <romain.ruaud@smile.fr>
2020-04-03 16:57:39 +0200

    Implement new percolator query

--- Model/Percolator.php
+++ Model/Percolator.php
@@ -17,6 +17,7 @@ use Smile\ElasticsuiteCore\Api\Client\ClientConfigurationInterface;
 use \Magento\Store\Model\StoreManagerInterface;
 use \Smile\ElasticsuiteCore\Api\Index\IndexOperationInterface;
 use Smile\ElasticsuiteCore\Client\ClientBuilder;
+use Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Response\QueryResponseFactory;
 use \Smile\ElasticsuiteTargetRule\Model\Indexer\TargetRule\Percolator\Datasource\PercolatorData;
 use \Psr\Log\LoggerInterface;
 
@@ -55,26 +56,34 @@ class Percolator
     protected $indexIdentifier;
 
     /**
+     * @var QueryResponseFactory
+     */
+    private $responseFactory;
+
+    /**
      * Percolator constructor.
      *
-     * @param ClientConfigurationInterface $clientConfiguration Client configuration factory.
-     * @param ClientBuilder                $clientBuilder       ES client builder.
-     * @param StoreManagerInterface        $storeManager        Store manager
-     * @param IndexOperationInterface      $indexManager        ES index manager
-     * @param LoggerInterface              $logger              Logger
-     * @param string                       $indexIdentifier     ES index name/identifier (as defined in XMLs)
+     * @param ClientConfigurationInterface $clientConfiguration  Client configuration factory.
+     * @param ClientBuilder                $clientBuilder        ES client builder.
+     * @param StoreManagerInterface        $storeManager         Store manager
+     * @param IndexOperationInterface      $indexManager         ES index manager
+     * @param QueryResponseFactory         $queryResponseFactory Search Query Response Factory
+     * @param LoggerInterface              $logger               Logger
+     * @param string                       $indexIdentifier      ES index name/identifier (as defined in XMLs)
      */
     public function __construct(
         ClientConfigurationInterface $clientConfiguration,
         ClientBuilder $clientBuilder,
         StoreManagerInterface $storeManager,
         IndexOperationInterface $indexManager,
+        QueryResponseFactory $queryResponseFactory,
         LoggerInterface $logger,
-        $indexIdentifier = 'catalog_product'
+        $indexIdentifier = \Smile\ElasticsuiteTargetRule\Model\Indexer\TargetRule\Percolator::INDEX_IDENTIFIER
     ) {
         $this->client          = $clientBuilder->build($clientConfiguration->getOptions());
         $this->storeManager    = $storeManager;
         $this->indexManager    = $indexManager;
+        $this->responseFactory = $queryResponseFactory;
         $this->logger          = $logger;
         $this->indexIdentifier = $indexIdentifier;
     }
@@ -90,46 +99,42 @@ class Percolator
     public function getMatchingRuleIds($productId, $storeId = null)
     {
         $ruleIds = [];
-
         if (null === $storeId) {
             $storeId = $this->storeManager->getStore()->getId();
         }
+
         /** @var \Smile\ElasticsuiteCore\Api\Index\IndexInterface $index */
-        $index = $this->indexManager->getIndexByName($this->indexIdentifier, $storeId);
+        $index        = $this->indexManager->getIndexByName($this->indexIdentifier, $storeId);
+        $productIndex = $this->indexManager->getIndexByName('catalog_product', $storeId);
 
+        $percolatorQuery['percolate'] = [
+            'field' => '_targetrule.query',
+            'index' => $productIndex->getName(),
+            'type'  => $productIndex->getDefaultSearchType()->getName(),
+            'id'    => $productId,
+        ];
+
+        $percolatorFilter['query']['bool']['must'] = [
+            ['term' => ['_targetrule.percolator_type' => PercolatorData::PERCOLATOR_TYPE]],
+            // Also done in \Magento\TargetRule\Model\Index::getRuleCollection
+            // but limits the amount of requests/data exchanged.
+            ['term' => ['_targetrule.is_active' => true]],
+            $percolatorQuery,
+        ];
+
+        \Magento\Framework\Profiler::start('ES:Percolate Document');
         try {
-            $matches = $this->client->percolate(
-                [
-                    'index' => $index->getName(),
-                    'type'  => 'product',
-                    'id'    => $productId,
-                    'body'  => [
-                        'filter' => [
-                            'and' => [
-                                ['term' => ['percolator_type' => PercolatorData::PERCOLATOR_TYPE]],
-                                // Also done in \Magento\TargetRule\Model\Index::getRuleCollection
-                                // but limits the amount of requests/data exchanged.
-                                ['term' => ['is_active' => true]],
-                            ],
-                        ],
-                        'percolate_format' => 'ids',
-                    ],
-                ]
-            );
-
-            // $this->logger->debug('Percolator matches id -------------');
-            // $this->logger->debug(print_r($matches, true));
-            foreach ($matches['matches'] as $match) {
-                $percolationData = $this->client->get(
-                    ['index' => $index->getName(), 'type' => '.percolator', 'id' => $match['_id']]
-                );
-                $ruleIds[]       = (int) $percolationData['_source']['rule_id'];
-            }
-
-            // $this->logger->debug('Matches rule ids  -------------');
-            // $this->logger->debug(print_r($ruleIds, true));
+            $searchParams   = ['index' => $index->getName(), 'type' => '_doc', 'body' => $percolatorFilter];
+            $searchResponse = $this->client->search($searchParams);
         } catch (\Exception $e) {
+            $searchResponse = [];
             $this->logger->error($e->getMessage());
+        }
+        \Magento\Framework\Profiler::stop('ES:Percolate Document');
+
+        $response = $this->responseFactory->create(['searchResponse' => $searchResponse]);
+        foreach ($response->getIterator() as $document) {
+            $ruleIds[] = $document->getId();
         }
 
         return $ruleIds;
